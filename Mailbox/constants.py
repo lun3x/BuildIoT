5347e5f8c66a47a8387e59b61f5d0da5a0583880
Louis Wyborn <lw15771@my.bristol.ac.uk>
2017-02-01 19:55:17 +0000

    Removed unnecessary and secret tokens

--- Mailbox/constants.py
+++ Mailbox/constants.py
@@ -1,10 +1,11 @@
 import tweepy
+from secret_constants import *
 
 # constants
 mailboxHandle = 'youvegotmailbox'
 whitelistUsers = ['ahmerb2', 'lun3x']
 
 # set up API
-auth = tweepy.OAuthHandler("1rapGRTeXdypArOAa10uxA1TS", "PxUathgqmHquKAFrKj2a4PAQXizGCogVUFy1eHKQI2SWrB1s5J")
-auth.set_access_token("825312532302815232-0CNFh0KwJCthpAlRgpq2NeSXcycp2Pt", "Gs5tl2W1IVpCHpCTPvUp0w28ZwBCBw9Pji8onYmqeJhIQ")
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
+auth.set_access_token(access_token, access_secret_token)
 api = tweepy.API(auth)
--- Mailbox/mailbox_pi.py
+++ Mailbox/mailbox_pi.py
@@ -10,17 +10,21 @@ from constants import *
 class MailboxPi:
 
     def __init__(self):
+        # get refernces to GPIO pins
         self.lock = Servo(17)
         self.flag = Servo(18)
+        self.floor = Button(6)
+
+        # set inital values of flag down, door locked
         self.flag.min()
         self.lock.value = -0.2
-        self.floor = Button(6)
+
+        # set event handlers for raising and lowering the flag
         self.floor.when_released = self._lowerFlag
         self.floor.when_pressed = self._liftFlag
-        self.mailInBox = False
 
     def hasMail(self):
-        return self.floor.is_pressed
+        return self.floor.is_pressed # true or false
 
     def lockDoor(self):
         self.lock.value = -0.2
@@ -30,10 +34,10 @@ class MailboxPi:
 
     def _liftFlag(self):
         print("You've got mail")
-        api.update_status("You've got mail! -- {0}".format(time.strftime('%X %x')))
-        self.flag.mid()
+        self.flag.mid() # raise flag
+        api.update_status("You've got mail! -- {0}".format(time.strftime('%X %x'))) # tweet confirmation
 
     def _lowerFlag(self):
         print("You've emptied your mail")
-        api.update_status("You've emptied your mail -- {0}".format(time.strftime('%X %x')))
-        self.flag.min()
+        self.flag.min() # lower flag
+        api.update_status("You've emptied your mail -- {0}".format(time.strftime('%X %x'))) # tweet confirmation
